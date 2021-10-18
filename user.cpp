0b1b5e5ceb8ac520ac3a031d9f517c6e4c24ebc3
AndreyZolotykh <59248314+AndreyZolotykh@users.noreply.github.com>
2021-10-19 00:26:06 +0300

    Добавлены классы User и Message

---
+++ user.cpp
@@ -0,0 +1,25 @@
+﻿#include <string>
+#include <iostream>
+#include <vector>
+#include "user.h"
+using namespace std;
+
+string User::getUserLogin() const
+{
+	return _login;
+}
+
+void User::setUserLogin(string login)
+{
+	_login = login;
+}
+
+string User::getUserPassword() const
+{
+	return _password;
+}
+
+void User::setUserPassword(string password)
+{
+	_password = password;
+}
---
+++ user.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include <string>
+#include <vector>
+using namespace std;
+
+class User
+{
+public:
+	User() = default;
+	~User() = default;
+
+	User(const string& user_login, const string& user_password) : _login(user_login), _password(user_password) {}
+
+	void setUserLogin(string login);
+	string getUserLogin() const;
+	void setUserPassword(string password);
+	string getUserPassword() const;
+
+private:
+	string _login; // переменная с логином
+	string _password; // переменная с паролем
+};
